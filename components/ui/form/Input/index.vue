b8be46d3d178ee348672b0e0bc36edda8a2cd0f8
romain paris <romainparis.ca@gmail.com>
2024-03-09 17:13:22 -0500

    Form validation

---
+++ components/ui/form/Input/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const { label, name, type } = defineProps({
+  label: String,
+  name: String,
+  type: {
+    type: String,
+    default: "text",
+  },
+});
+const id = useId();
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 mb-4">
+    <label :for="id">{{ label }}</label>
+    <VeeField
+      :type="type"
+      :name="name"
+      :id="id"
+      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    />
+    <VeeErrorMessage :name="name" class="text-sm text-red-500" />
+  </div>
+</template>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { toTypedSchema } from "@vee-validate/zod";
+import { z } from "zod";
+
 useHead({
   title: "Nuxt Boilerplate - Login",
   meta: [{ name: "description", content: "Login page" }],
@@ -7,11 +10,35 @@ useHead({
     class: "login",
   },
 });
+// Minimum 8 characters, at least one uppercase letter, one lowercase letter, one number and one special character
+const passwordValidation = new RegExp(
+  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/,
+);
+
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6).regex(passwordValidation, {
+    message:
+      "Minimum 6 characters, at least one uppercase letter, one lowercase letter, one number and one special character",
+  }),
+});
+type Schema = z.infer<typeof schema>;
+const validationSchema = toTypedSchema(schema);
+const onSubmit = (values: Schema) => {
+  console.log(values);
+};
 </script>
 
 <template>
-  <HeroTitle title="Login" definition="This is the login page." />
-  <section class="flex flex-col max-w-[350px]"></section>
+  <div>
+    <NuxtPage />
+    <HeroTitle title="Login" definition="This is the login page." />
+    <section class="flex flex-col max-w-[350px]">
+      <VeeForm :validation-schema="validationSchema" @submit="onSubmit">
+        <UiFormInput label="Email" name="email" />
+        <UiFormInput label="Password" name="password" type="password" />
+        <button class="px-4 py-1 border rounded">Send</button>
+      </VeeForm>
+    </section>
+  </div>
 </template>
-
-<NuxtPage />
