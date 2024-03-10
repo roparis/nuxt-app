f4f471497409969e0fcf36c49d29e3ac23abf4fc
romain paris <romainparis.ca@gmail.com>
2024-03-09 20:25:49 -0500

    add prisma

---
+++ server/trpc/router.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+import { publicProcedure, router } from "../trpc";
+import { userRouter } from "~/server/trpc/routers/userRouter";
+
+export const appRouter = router({
+  user: userRouter,
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
